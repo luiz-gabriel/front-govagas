4bbe130ca6ef643dcf5fa9942a0b9a52c1590732
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-13 12:35:10 -0300

    Refatoração e Formatação
    Refatoração de código, reorganização do componentes de cards, e estruturação do serviço dos cards!

--- src/app/components/card-vaga/card-vaga.component.ts
+++ src/app/components/card-vaga/card-vaga.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Card } from 'src/app/model/card';
+import { CardService } from 'src/app/services/card.service';
 
 @Component({
   selector: 'app-card-vaga',
   templateUrl: './card-vaga.component.html',
-  styleUrls: ['./card-vaga.component.scss']
+  styleUrls: ['./card-vaga.component.scss'],
 })
 export class CardVagaComponent {
+  public cards!: Array<Card>;
 
+  constructor(private cardsService: CardService) {
+    this.cards = cardsService.getCard();
+  }
 }
